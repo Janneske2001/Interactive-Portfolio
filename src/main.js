e5ee1a35556214cc85b5878bc166179713cfdf03
Janneske2001 <janneske2001@gmail.com>
2026-03-12 18:58:27 +0100

    Cubes based on List, Grow on Hover, Clickable

--- src/main.js
+++ src/main.js
@@ -2,18 +2,23 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import './style.css'
 
+const raycaster = new THREE.Raycaster()
+const mouse = new THREE.Vector2()
+
+let hoveredCube = null
+
 // Scene
 const scene = new THREE.Scene()
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
- 75,
- window.innerWidth / window.innerHeight,
- 0.1,
- 1000
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
 )
 
-camera.position.set(0, 8, -4)
+camera.position.set(0, 8, 4)
 // camera.lookAt(0, 0, 0)
 
 // Renderer + Controls
@@ -31,8 +36,7 @@ controls.maxPolarAngle = Math.PI / 2
 // Prevent Panning Away
 controls.enablePan = false
 // Lock Target
-controls.target.set(0, 1, 0)
-
+controls.target.set(0, 0.5, 0)
 controls.update()
 
 
@@ -62,45 +66,180 @@ cube.position.set(0,0,0)
 // scene.add(cube)
 
 
-// GRID OF CUBES
-const cubes = []
-
-const gridSize = 3
-const xSpacing = 4
-const ySpacing = 3
-
-for (let x = 0; x < gridSize; x++) {
-  for (let z = 0; z < gridSize; z++) {
-
-    const cube = new THREE.Mesh(geometry, material)
-
-    cube.position.x = (x - gridSize / 2) * xSpacing + (xSpacing / 2)
-    cube.position.z = (z - gridSize / 2) * ySpacing + (ySpacing / 2)
-    cube.position.y = 1
-
-    scene.add(cube)
-    cubes.push(cube)
-
+// ------------------------------     PROJECT DATA
+const projects = [
+  {
+    title: "NOVA Breach",
+    type: "game",
+    link: "https://example.com/nova",
+    texture: null
+  },
+  {
+    title: "3D Print Gearbox",
+    type: "3d",
+    link: "https://example.com/gearbox",
+    texture: null
+  },
+  {
+    title: "Portfolio Website",
+    type: "web",
+    link: "https://example.com/portfolio",
+    texture: null
+  },
+  {
+    title: "Hardware Mod",
+    type: "hardware",
+    link: "https://example.com/mod",
+    texture: null
+  },
+  {
+    title: "TEST1",
+    type: "hardware",
+    link: "https://example.com/mod",
+    texture: null
+  },
+  {
+    title: "TEST2",
+    type: "hardware",
+    link: "https://example.com/mod",
+    texture: null
+  },
+  {
+    title: "TEST3",
+    type: "hardware",
+    link: "https://example.com/mod",
+    texture: null
+  },
+  {
+    title: "TEST4",
+    type: "hardware",
+    link: "https://example.com/mod",
+    texture: null
+  },
+  {
+    title: "TEST5",
+    type: "hardware",
+    link: "https://example.com/mod",
+    texture: null
+  },
+  {
+    title: "TEST6",
+    type: "hardware",
+    link: "https://example.com/mod",
+    texture: null
   }
-}
+]
+
+
+// GRID OF CUBES
+// const cubes = []
+
+// const gridSize = 3
+// const xSpacing = 4
+// const zSpacing = 3
+
+// for (let x = 0; x < gridSize; x++) {
+//   for (let z = 0; z < gridSize; z++) {
+
+//     const cube = new THREE.Mesh(geometry, material)
+
+//     cube.position.x = (x - gridSize / 2) * xSpacing + (xSpacing / 2)
+//     cube.position.z = (z - gridSize / 2) * zSpacing + (zSpacing / 2)
+//     cube.position.y = 1
+
+//     scene.add(cube)
+//     cubes.push(cube)
+
+//   }
+// }
+
+
+// Cubes With Data
+const cubes = []
+const xSpacing = 3
+const zSpacing = 3
+
+projects.forEach((project, index) => {
+
+  const cube = new THREE.Mesh(geometry, material)
+
+  const rowLength = 4
+
+  const x = index % rowLength
+  const z = Math.floor(index / rowLength)
+
+  cube.position.x = (x - rowLength / 2) * xSpacing + (xSpacing / 2)
+  cube.position.z = (z - rowLength / 2) * zSpacing + (zSpacing)
+  cube.position.y = 1
+
+  cube.userData = project
+
+  scene.add(cube)
+  cubes.push(cube)
+
+})
+
 
 // Light
 const light = new THREE.PointLight(0xffffff, 10)
 light.position.set(5, 5, 5)
 scene.add(light)
 
+
+window.addEventListener("mousemove", (event) => {
+
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+
+})
+
+
 // Animation
 function animate() {
+  raycaster.setFromCamera(mouse, camera)
 
- requestAnimationFrame(animate)
+  const intersects = raycaster.intersectObjects(cubes)
+
+  if (intersects.length > 0) {
+    const cube = intersects[0].object
+    if (hoveredCube !== cube) {
+      if (hoveredCube) {
+        hoveredCube.scale.set(1,1,1)
+      }
+
+      hoveredCube = cube
+      cube.scale.set(1.2,1.2,1.2)
+    }
+
+  } else {
+    if (hoveredCube) {
+      hoveredCube.scale.set(1,1,1)
+    }
+
+    hoveredCube = null
+
+  }
+
+  requestAnimationFrame(animate)
 
 //  cube.rotation.x += 0.01
 //  cube.rotation.y += 0.01
 
- controls.update()
+  controls.update()
 
- renderer.render(scene, camera)
+  renderer.render(scene, camera)
 
 }
+
+window.addEventListener("click", () => {
+
+  if (hoveredCube) {
+    const project = hoveredCube.userData
+    console.log("Opening project:", project.title)
+    window.open(project.link, "_blank")
+  }
+
+})
+
 
 animate()
